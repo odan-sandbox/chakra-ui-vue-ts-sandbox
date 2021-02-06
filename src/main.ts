12d7d40ae568818c86caf38f8d4e7d0113fef073
odan <odan3240@gmail.com>
2021-02-07 00:45:04 +0900

    add chakra-ui

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import Vue from "vue";
+import Chakra, { CThemeProvider } from "@chakra-ui/vue";
 import App from "./App.vue";
 
 Vue.config.productionTip = false;
 
+Vue.use(Chakra);
+
 new Vue({
-  render: h => h(App)
+  render: h => h(CThemeProvider, [h(App)])
 }).$mount("#app");
